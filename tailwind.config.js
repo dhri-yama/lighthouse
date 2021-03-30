2ef1e298f4c64ff4773aa5c14849678ef47a1466
Sagar Singh <dhrijethi@gmail.com>
2021-03-30 11:31:59 +0530

    added purgecss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: ["./src/**/*.js"],
+  purge: {
+    enabled: true,
+    content: ["./src/**/*.jsx"],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
